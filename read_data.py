a9bee43449c8896e278720d8c90b6e721476799a
m-n64 <162654384+m-n64@users.noreply.github.com>
2025-01-30 14:18:16 +0000

    edited read_data.py

--- read_data.py
+++ read_data.py
@@ -6,11 +6,17 @@ for year in os.listdir('./data'):
     for month in os.listdir(f'./data/{year}'):
 
         count = 0
-
-        with open(f'./data/{year}/{month}') as jsonFile:
-            data = json.load(jsonFile)['response']['docs']
-
-            for article in data:
-                print(article)
+        try:
+            with open(f'./data/{year}/{month}') as jsonFile:
+                response = json.load(jsonFile)
+                data = response['response']['docs']
+                for article in data:
+                    try: 
+                        if (int(article['print_page']) == 1) or (article['print_section'] == 'A'):
+                            count += 1
+                    except KeyError:
+                        pass
             
-        print(f'{month}/{year} has {count} usable articles.')
+            print(f'Within {month}, {count} of {len(data)} were front page articles [{round(100*(count/len(data)))}%]')
+        except json.JSONDecodeError as e:
+            print(f'---------\n{month}\n{e}\n----------')    
