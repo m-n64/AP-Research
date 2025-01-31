fa2c9b99d141307672dadb3d6531a8f782897a8b
m-n64 <162654384+m-n64@users.noreply.github.com>
2025-01-31 16:55:50 +0000

    Created Script to Grab Article Keys

--- filter_data/fpage_stats.py
+++ filter_data/fpage_stats.py
@@ -1,51 +1,57 @@
 import os
 import json
+import sys
+from pathlib import Path
 
-def fpage_stats():
+sys.path.append(str(Path(__file__).parent.parent))
+from Useful.percent import percent
+
+def fpage_stats(stats: bool = False):
 
     key_files = []
     extra_files = []
     total_results = 0
     valid_results = 0
+    article_keys = {}
 
-    for year in os.listdir('.data'):
+    for year in os.listdir('./data'):
         for file in os.listdir(f'./data/{year}'):
             
             fpages = 0
 
             date = file.split('.')[0]
-            usable = False
 
-            with open(f'./data/{year}{file}', 'r') as jsonFile:
+            with open(f'./data/{year}/{file}', 'r') as jsonFile:
                 
                 response = json.load(jsonFile)['response']
                 data = response['docs']
                 results = int(response['meta']['hits'])
-
+                article_keys[file] = []
                 for article in data:
-                    if int(article['print_page']) == 1:
-                        fpages += 1
-                        usable = True
+                    try:
+                        if int(article['print_page']) == 1:
+                            fpages += 1
+                            article_keys[file].append(data.index(article))
+                            
+                    except KeyError:
+                        pass
             
             valid_results += fpages
             total_results += results
             
-            if usable: key_files.append(file)
+            if fpages > 0: key_files.append(file)
             else: extra_files.append(file)
-
-            print(f'---\n{date} -- {fpages}/{results} [{percent(fpages, results, 2)}%]\n---')
-
-    print('**********')
-    print(f'TOTAL -- {valid_results}/{total_results}')
-    print('**********')
+            if stats:
+                print(f'---\n{date} -- {fpages}/{results} [{percent(fpages, results, 2)}%]\n---')
+            
+    
+    print(f'TOTAL -- {valid_results}/{total_results} [{percent(valid_results, total_results, 2)}%]')
 
     total_files = key_files + extra_files
-
+        
     print(f'Usable Files: {percent(len(key_files), len(total_files), 2)}%')
-    print(key_files)
-    print(f'Unusable File: {len(extra_files, len(total_files), 2)}%')
-    print(extra_files)
-
+    print(f'Unusable Files: {percent(len(extra_files), len(total_files), 2)}%')
+    return article_keys
 
 if __name__ == '__main__':
-    fpage_stats()
+    fpage_stats(True)
