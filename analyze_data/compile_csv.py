17375b32be9da7aeccebf3ac4d7332176ff901ae
m-n64 <marcus.needham06@gmail.com>
2025-02-11 17:07:21 -0500

    changed to write_rows

--- analyze_data/compile_csv.py
+++ analyze_data/compile_csv.py
@@ -8,23 +8,22 @@ try:
 except IndexError:
     latest_year = 1961
     
-for folder in data:
-    if folder.year < latest_year:
-        pass
-    else:    
-        with open(f'./csv_tables/{folder.year}.csv', 'w', encoding='utf-8') as csvFile:
-            for file in folder.files:  
-                file.get_data()
-                    
-                headers = list(file.fpages[0].to_dict().keys())
-                writer = csv.DictWriter(csvFile, fieldnames=headers)
+with open(f'./data.csv', 'w', encoding='utf-8') as csvFile:
+    
+    has_header = False
+    for folder in data:
+        for file in folder.files:  
+            file.get_data()
+            headers = list(file.fpages[0].to_dict().keys())
+            writer = csv.DictWriter(csvFile, fieldnames=headers)
+            
+            if has_header == False:
                 writer.writeheader()
-                writer.writerows([article.to_dict() for article in file.fpages])
-                print('Data Entered')
+                has_header = True
+            
+            writer.writerows([article.to_dict() for article in file.fpages])
+            print('Data Entered')
 
                         
 
-
-
-                
             
